610a6a84132d8ef0f1885448b44f4db944c39f5f
eventstubsol <eventstubsol@gmail.com>
2021-09-22 19:22:34 +0530

    Dashboard Fix

--- resources/views/eventee/dashboard.blade.php
+++ resources/views/eventee/dashboard.blade.php
@@ -2,6 +2,10 @@
 @section('styles')
     @include("includes.styles.datatables")
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 @endsection
 
 @section('page_title')
@@ -31,7 +35,7 @@
                     </div>
                 </div>
                 @php
-                    $eventcount = App\EventSession::where('user_id',Auth::id())->where('created_at','like','%'.date('Y-m-d H:i:a').'%')->count();
+                    $eventcount = App\EventSession::where('user_id',Auth::id())->where('created_at','like','%'.date('Y-m-d').'%')->count();
                 @endphp
                 <div class="col-6">
                     <div class="text-right">
@@ -81,22 +85,38 @@
                     </div>
                 </div>
             </div> <!-- end row-->
-            <button class="btn btn-primary mt-3 mx-auto btn-block" id="download-login-logs">Event Logs</button>
+            
         </div> <!-- end widget-rounded-circle-->
     </div>
 </div>
 @php
-    $eventee = App\EventSession::where('user_id',Auth::id())->orderBy('created_at','desc')->limit(5)->get();
+    $eventee = App\Event::where('user_id',Auth::id())->orderBy('created_at','desc')->limit(5)->get();
 @endphp
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 22rem;margin-left:1rem">
     <div class="card-body">
-        <h5 class="card-title">Last Events</h5>
-        @if(count(App\EventSession::where('user_id',Auth::id())->get()) > 0)
-            <ul class="list-group list-group-flush">
+        <h5 class="card-title">Events</h5>
+        @if(count(App\Event::where('user_id',Auth::id())->get()) > 0)
+            <table class="table table-hover">
+                <tr>
+                    <th>Event</th>
+                    <th>Status</th>
+                </tr>
                 @foreach($eventee as $event)
-                <li class="list-group-item" data-id="{{ $event->id }}" style="cursor: pointer">{{ $event->name }}</li>
+                <tr>
+                   
+                        <td><a href="{{ route('event.Dashboard',['id'=>encrypt( $event->id )]) }}" style="text-decoration: none">{{ $event->name }}</a></td>
+                        <td>
+                            @if ($event->end_date < Carbon\Carbon::today())
+                                <span style="color: red">○ &nbsp;Expired</span>
+                            @else
+                                <span style="color: green">○ &nbsp;Active</span>
+                            @endif
+                        </td>
+                   
+                </tr>
                 @endforeach
-            </ul>
+                
+            </table>
         @else
          <h5>No data available</h5>
         @endif
