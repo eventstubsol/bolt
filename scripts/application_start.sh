fe01077aca857810907431547a70568964a15756
vipin gupta <vipinguptaei08@gmail.com>
2021-10-31 01:39:37 +0530

    codedeploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,12 +2,12 @@
 
 # give permission to the files inside /secure_docs directory
 
-sudo chmod -R 775 /var/www/test
-sudo chown -Rf www-data:www-data /var/www/test
+sudo chmod -R 775 /var/www/html
+sudo chown -Rf www-data:www-data /var/www/html
 
 # navigate into current working directory
 
-cd /var/www/test
+cd /var/www/html
 
 # install node modules
 
