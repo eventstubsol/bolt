131db4bdbf2bd1ce7d59daa7f6fad5a809e1b62f
eventstubsol <eventstubsol@gmail.com>
2021-11-13 19:55:43 +0530

    custom error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,21 @@
-@extends('errors::minimal')
+@extends('layouts.auth')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('title')
+Not Found
+@endsection
+
+@section('form')
+
+<div class="jumbotron">
+    <center>
+        <h1 class="display-4"><span style="color: red">404! Not Found</span></h1>
+        <p class="lead">The page You Are Looking For Doesnot Seem To Be Existing</p>
+        <hr class="my-4">
+        <p>Please Try Some Valid Address</p>
+        {{-- <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p> --}}
+    </center>
+  </div>
+
+@endsection
