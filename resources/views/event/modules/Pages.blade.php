add956b5a503ebfc268a85095c0585d5ec11fe49
shubh-palan <shubhpalan@gmail.com>
2021-11-16 01:22:44 +0530

    videosdk link to pages

--- resources/views/event/modules/Pages.blade.php
+++ resources/views/event/modules/Pages.blade.php
@@ -72,6 +72,9 @@
                     @elseif($link->type === "photobooth")
                         <div title="{{ $link->name  }}"  class="photobooth positioned area" data-link="photo-booth" data-capture="{{$link->to}}" data-gallery="{{$link->url}}" style="{{ areaStyles($area) }}">    
                         </div>
+                    @elseif($link->type === "videosdk")
+                        <div title="{{ $link->name  }}" data-toggle="modal"  data-target="#videosdk_modal" class="videosdk positioned " data-link="videosdk" data-meeting="{{$link->to}}" style="{{ areaStyles($area) }}">    
+                        </div>
                     @else
                         @if($to ==="FAQ")
                             <a class="positioned" data-toggle="modal" data-target="#faqs-modal"
@@ -133,4 +136,21 @@
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
+</div>
+
+
+<div data-backdrop="static"  class="modal fade embed-modal slido-container-modal" id="videosdk_modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            <div class="modal-body">
+                <div class="position-relative">
+                    <div style="padding-bottom: {{ AUDI_IMAGE_ASPECT }}%"></div>
+                    <div id="videosdk-session-content" class="positioned fill" >
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
