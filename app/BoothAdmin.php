33570435927c7454dcdc406288378f232576fcff
shubh-palan <shubhpalan@gmail.com>
2021-11-20 15:16:36 +0530

    booth admin fix

--- app/BoothAdmin.php
+++ app/BoothAdmin.php
@@ -32,7 +32,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class BoothAdmin extends Model
 {
     use SoftDeletes;
-    // use UUID;
+    use UUID;
     public $incrementing = false;
     protected $fillable = [ "booth_id", "user_id" ];
 }
