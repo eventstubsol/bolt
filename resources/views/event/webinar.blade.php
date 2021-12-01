1db8fba099a378bc5763b75e48da247455c52628
shubh-palan <shubhpalan@gmail.com>
2021-12-01 16:40:32 +0530

    zoom sdk update

--- resources/views/event/webinar.blade.php
+++ resources/views/event/webinar.blade.php
@@ -2,8 +2,8 @@
 <head>
     <title>Zoom</title>
     <meta charset="utf-8" />
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.9.1/css/bootstrap.css" />
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.9.1/css/react-select.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.9.9/css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.9.9/css/react-select.css" />
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <style>
@@ -26,13 +26,13 @@
     </style>
 </head>
 <body>
-<script src="https://source.zoom.us/1.9.1/lib/vendor/react.min.js"></script>
-<script src="https://source.zoom.us/1.9.1/lib/vendor/react-dom.min.js"></script>
-<script src="https://source.zoom.us/1.9.1/lib/vendor/redux.min.js"></script>
-<script src="https://source.zoom.us/1.9.1/lib/vendor/redux-thunk.min.js"></script>
-<script src="https://source.zoom.us/1.9.1/lib/vendor/jquery.min.js"></script>
-<script src="https://source.zoom.us/1.9.1/lib/vendor/lodash.min.js"></script>
-<script src="https://source.zoom.us/zoom-meeting-1.9.1.min.js"></script>
+<script src="https://source.zoom.us/1.9.9/lib/vendor/react.min.js"></script>
+<script src="https://source.zoom.us/1.9.9/lib/vendor/react-dom.min.js"></script>
+<script src="https://source.zoom.us/1.9.9/lib/vendor/redux.min.js"></script>
+<script src="https://source.zoom.us/1.9.9/lib/vendor/redux-thunk.min.js"></script>
+<script src="https://source.zoom.us/1.9.9/lib/vendor/jquery.min.js"></script>
+<script src="https://source.zoom.us/1.9.9/lib/vendor/lodash.min.js"></script>
+<script src="https://source.zoom.us/zoom-meeting-1.9.9.min.js"></script>
 <script src="{{ asset("js/zoom/tool.js") }}"></script>
 <script src="{{ asset("js/zoom/meeting.js") }}"></script>
 </body>
