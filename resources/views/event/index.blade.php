1b22f308d92825aa517d724cb0d04b1d47da9328
shubh-palan <shubhpalan@gmail.com>
2021-12-02 18:01:05 +0530

    cache

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -485,11 +485,11 @@ $user = Auth::user();
     @include("includes.styles.fileUploader")
     <!-- Custom -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-    <link href="{{ asset('/dflip/css/dflip.css') }}?cb=21872367624367" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/dflip/css/themify-icons.css') }}?cb=21872367624367" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}?cb=21872367624367" type="text/css">
-    <link rel="stylesheet" href="{{ asset('event-assets/css/app.css') }}?cb=21872367624367">
-    <link href="{{ asset('assets/css/custom.css') }}?v=21872367624367" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/dflip/css/dflip.css') }}?cb=21872367624361" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/dflip/css/themify-icons.css') }}?cb=21872367624361" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}?cb=21872367624361" type="text/css">
+    <link rel="stylesheet" href="{{ asset('event-assets/css/app.css') }}?cb=21872367624361">
+    <link href="{{ asset('assets/css/custom.css') }}?v=21872367624361" rel="stylesheet" type="text/css" />
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id={{ api('GA_TRACKING_ID',$event_id) }}"></script>
     @php
@@ -860,15 +860,15 @@ $user = Auth::user();
         window.config = config;
     </script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script src="{{ asset('assets/js/vendor.min.js') }}?cb=21872367624367"></script>
-    <script src="{{ asset('assets/js/app.min.js') }}?cb=21872367624367"></script>
-    <script src="{{ asset('event-assets/js/routie.min.js') }}?cb=21872367624367"></script>
-    <script src="{{ asset('event-assets/js/app.js') }}?cb=21872367624367"></script>
-    <script src="{{ asset('/js/chat/app.js') }}?cb=21872367624367"></script>
-    <!-- <script src="{{ asset('/js/by-laws/App.js') }}?cb=21872367624367"></script> -->
-    <script src="{{ asset('/js/profile/index.js') }}?cb=21872367624367"></script>
-    <script src="{{ asset('event-assets/YouTubePopUp/YouTubePopUp.jquery.js') }}?cb=21872367624367"></script>
-    <script src="{{ asset('event-assets/YouTubePopUp/PopupInit.js') }}?cb=21872367624367"></script>
+    <script src="{{ asset('assets/js/vendor.min.js') }}?cb=21872367624361"></script>
+    <script src="{{ asset('assets/js/app.min.js') }}?cb=21872367624361"></script>
+    <script src="{{ asset('event-assets/js/routie.min.js') }}?cb=21872367624361"></script>
+    <script src="{{ asset('event-assets/js/app.js') }}?cb=21872367624361"></script>
+    <script src="{{ asset('/js/chat/app.js') }}?cb=21872367624361"></script>
+    <!-- <script src="{{ asset('/js/by-laws/App.js') }}?cb=21872367624361"></script> -->
+    <script src="{{ asset('/js/profile/index.js') }}?cb=21872367624361"></script>
+    <script src="{{ asset('event-assets/YouTubePopUp/YouTubePopUp.jquery.js') }}?cb=21872367624361"></script>
+    <script src="{{ asset('event-assets/YouTubePopUp/PopupInit.js') }}?cb=21872367624361"></script>
     @if (isOpenForPublic('polls'))
         @include("event.poll")
     @endif
