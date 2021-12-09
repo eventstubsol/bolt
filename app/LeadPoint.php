8a09d5982d9c47a5ec850f10930995a4dfddbc7c
swarnadeep-pramanick <swarnadeeppramanick2@gmail.com>
2021-12-09 19:08:18 +0530

    Leaderboard Setting

---
+++ app/LeadPoint.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\UUID;
+class LeadPoint extends Model
+{
+    //
+    use UUID;
+    public $incrementing = false;
+    protected $guarded = [];
+
+}
