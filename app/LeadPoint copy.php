e5a7c7993c427ddc27c5bc50ace2f466a5a0b46e
shubh-palan <shubhpalan@gmail.com>
2021-12-10 12:34:02 +0530

    Leadpoint.php

---
+++ app/LeadPoint copy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\UUID;
+class LeadPoint extends Model
+{
+    //
+    use UUID;
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $table = 'lead_point';
+    
+
+
+    
+}
