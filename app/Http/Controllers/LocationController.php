44885cd3c6b2b0d1f78085638ddef53f87734b13
swarnadeep-pramanick <swarnadeeppramanick2@gmail.com>
2021-12-10 14:13:52 +0530

    changes in table

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\UserLocation;
 use Illuminate\Support\Facades\DB;
 use App\Booth;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class LocationController extends Controller
@@ -17,41 +18,41 @@ class LocationController extends Controller
         // return($type);
         switch($type){
             case 'Lobby':
-                UserLocation::where('user_id',Auth::id())->update(['current_status'=>0]);
+                UserLocation::where('user_id',Auth::id())->where('current_status',1)->update(['current_status'=>0]);
                 // DB::UPDATE("UPDATE user_locations SET current_status = ? WHERE user_id = ?",[0,Auth::id()]);
                 $user = UserLocation::create(['type'=>$type,'type_location'=>null,'user_id'=>Auth::id(),'current_status'=>1,'event_id'=>Auth::user()->event_id]);
                 
                 return response()->json(['code'=>200]);
                 break;
             case 'Leaderboard':
-                UserLocation::where('user_id',Auth::id())->update(['current_status'=>0]);
+                UserLocation::where('user_id',Auth::id())->where('current_status',1)->update(['current_status'=>0]);
                 $user = UserLocation::create(['type'=>$type,'type_location'=>null,'user_id'=>Auth::id(),'current_status'=>1,'event_id'=>Auth::user()->event_id]);
                 return response()->json(['code'=>200]);
                 break;
             case 'Booth':
                 // return $typeloc;
-                UserLocation::where('user_id',Auth::id())->update(['current_status'=>0]);
+                UserLocation::where('user_id',Auth::id())->where('current_status',1)->update(['current_status'=>0]);
                 $booth = Booth::findOrFail($typeloc);
                 $user = UserLocation::create(['type'=>$type,'type_location'=>$booth->name,'user_id'=>Auth::id(),'current_status'=>1,'event_id'=>Auth::user()->event_id]);
                 return response()->json(['code'=>200]);
                 break;
             case 'Sessionroom':
-                UserLocation::where('user_id',Auth::id())->update(['current_status'=>0]);
+                UserLocation::where('user_id',Auth::id())->where('current_status',1)->update(['current_status'=>0]);
                 $user = UserLocation::create(['type'=>$type,'type_location'=>$typeloc,'user_id'=>Auth::id(),'current_status'=>1,'event_id'=>Auth::user()->event_id]);
                 return response()->json(['code'=>200]);
                 break;
             case 'page':
-                UserLocation::where('user_id',Auth::id())->update(['current_status'=>0]);
+                UserLocation::where('user_id',Auth::id())->where('current_status',1)->update(['current_status'=>0]);
                 $user = UserLocation::create(['type'=>$type,'type_location'=>$typeloc,'user_id'=>Auth::id(),'current_status'=>1,'event_id'=>Auth::user()->event_id]);
                 return response()->json(['code'=>200]);
                 break;
             case "Lounge":
-                UserLocation::where('user_id',Auth::id())->update(['current_status'=>0]);
+                UserLocation::where('user_id',Auth::id())->where('current_status',1)->update(['current_status'=>0]);
                 $user = UserLocation::create(['type'=>$type,'type_location'=>null,'user_id'=>Auth::id(),'current_status'=>1,'event_id'=>Auth::user()->event_id]);
                 return response()->json(['code'=>200]);
                 break;
             case "Attendees":
-                UserLocation::where('user_id',Auth::id())->update(['current_status'=>0]);
+                UserLocation::where('user_id',Auth::id())->where('current_status',1)->update(['current_status'=>0]);
                 $user = UserLocation::create(['type'=>$type,'type_location'=>null,'user_id'=>Auth::id(),'current_status'=>1,'event_id'=>Auth::user()->event_id]);
                 return response()->json(['code'=>200]);
                 break;
