c71c1e2867d9f43676108d3662544367dc9785cc
ranjeet singh <ranjeet.singh@sunandaei.com>
2021-12-14 12:43:10 +0530

    Chnages in schedular

---
+++ app/Http/Controllers/SchedularJobsController.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Http\Controllers;
+use http\Url;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
+use App\ScheduleNotification;
+use App\Events\NotificationEvent;
+use App\PushNotification;
+
+class SchedularJobsController extends Controller
+{
+  public function runJobs()
+  {
+    return null;
+  }
+
+
+  public function runJobs()
+  {
+    $schedules = ScheduleNotification::whereBetween('sending_time',[Carbon::now()->subMinutes(15)->format('H:i'),Carbon::now()->format('H:i')])
+        ->where('sending_date',Carbon::now()->format('Y-m-d'))
+        ->where('status',0)
+        ->get();    
+        // print_r(Carbon::now()->subMinutes(15)->format('H:i'));
+       if(count($schedules) > 0){
+           print_r($schedules);
+            foreach($schedules as $schedule){
+                $event = Event::findOrFail($schedule->event_id);
+                $notify = new PushNotification;
+                $notify->title = $schedule->title;
+                $notify->url = $schedule->url;
+                $notify->message = $schedule->message;
+                $notify->roles =$schedule->role;
+                $notify->event_id = $event->id;
+                if($notify->save()){
+                    event(new NotificationEvent($schedule->message,$schedule->title,$event->slug,$notify->id,$schedule->role,$schedule->url));
+                    $schedule->status = 1;
+                    $schedule->sent_on = Carbon::now()->format('Y-m-d H:i:s');
+                    $schedule->save();
+                }
+                
+            }
+            echo 1;
+       }
+       else{
+           echo 0;
+       }
+  }
+
+}
