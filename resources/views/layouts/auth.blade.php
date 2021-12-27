fb759c0d337d2a4c026eff13cb91235175abf887
shubh-palan <shubhpalan@gmail.com>
2021-12-27 18:42:08 +0530

    push

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
+    
     <link rel="shortcut icon" href="{{ assetUrl(getFieldId('favicon',$id)) }}?v=3">
     <!-- App css -->
     <link href="{{asset("assets/css/bootstrap.min.css")}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet">
