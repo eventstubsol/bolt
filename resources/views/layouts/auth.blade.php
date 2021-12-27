c3b3098437d9f98d69f0358f78f1fa9d5412fca2
swarnadeep-pramanick <swarnadeeppramanick2@gmail.com>
2021-12-27 19:59:14 +0530

    data add

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ assetUrl(getFieldId('favicon',$event_id)) }}?v=3">
+    
+    <link rel="shortcut icon" href="{{ assetUrl(getFieldId('favicon',$id)) }}?v=3">
     <!-- App css -->
     <link href="{{asset("assets/css/bootstrap.min.css")}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet">
     <link href="{{asset("assets/css/app.min.css")}}" rel="stylesheet" type="text/css" id="app-default-stylesheet">
