7ed1d4eda8072446a29166d527b190932fae35d9
swarnadeep-pramanick <swarnadeeppramanick2@gmail.com>
2022-01-25 13:26:47 +0530

    color fix

--- resources/views/includes/styles/datatables.blade.php
+++ resources/views/includes/styles/datatables.blade.php
@@ -1,3 +1,9 @@
+<style>
+    .dataTables_info{
+        color:white !important;
+    }
+</style>
+
 <link href="{{ asset("assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css")}}" rel="stylesheet" type="text/css" />
 <link href="{{ asset("assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css")}}" rel="stylesheet" type="text/css" />
 <link href="{{ asset("assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css")}}" rel="stylesheet" type="text/css" />
