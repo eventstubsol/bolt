973a172fae2f7c4f9ff5af059becd7490cd86f6a
Sourav Ball <sourav.ball@webskitters.com>
2022-02-03 17:49:07 +0530

    ui

--- resources/views/event/modules/networking.blade.php
+++ resources/views/event/modules/networking.blade.php
@@ -164,28 +164,33 @@
         object-fit: cover;
     }
 
+    .BigTableBox{
+        width: 50%;
+        margin: 0 auto;
+    }
+
     .bigTable{
-        width: 85%;
+        width: 75%;
     }
 
     .bigTopchair{
-        padding: 0 8%;
+        padding: 0 12%;
         justify-content: space-between;
     }
 
     .bigLeftchair{
         width: initial;
         transform: rotate(-90deg);
-        left: 12%;
+        left: 16%;
     }
 
     .bigRightchair{
         width: initial;
-        right: 12%;
+        right: 16%;
     }
 
     .bigBottomchair{
-        padding: 0 8%;
+        padding: 0 12%;
         justify-content: space-between;
     }
 
@@ -337,151 +342,10 @@
                 @endfor
             </div>
         @endforeach
-        {{-- <div class="table_Box">
-            <div class="TableBlock d-flex justify-content-between flex-column" data-toggle="modal" data-table="{{$table->id}}" data-target="#lounge_modal" data-meeting="{{$table->meeting_id}}">
-                <div>
-                    <h2>Seats: <span>2 Available<span></h2>
-                    <h2 class="mt-3">Seats: <span>2<span></h2>
-                </div>
-                <div class="mt-4">
-                    <h3 class="d-flex align-items-center"> 
-                        <span class="mr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" class="bi bi-person-check" viewBox="0 0 16 16">
-                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-                            </svg>
-                        </span> 
-                        Attendee User
-                    </h3>
-                </div>
-            </div>
-            <ul class="tob_Chair">
-                <li>
-                    <img src="{{asset("/assets/images/chair-svg.svg")}}" />
-                    <div class="chairBooking">
-                        <span>
-                            <img src="/assets/images/user_img.png" alt="">
-                        </span>
-                        <h4>User Name</h4>
-                    </div>
-                </li>
-                <li>
-                    <img src="{{asset("/assets/images/chair-svg.svg")}}" />
-                    <div class="chairBooking">
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-person" viewBox="0 0 16 16">
-                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                            </svg>
-                        </span>
-                        <h4>User Name</h4>
-                    </div>
-                </li>
-            </ul>
-            <ul class="bottom_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-        </div>
-        <div class="table_Box">
-            <div class="TableBlock d-flex justify-content-between flex-column" data-toggle="modal" data-table="{{$table->id}}" data-target="#lounge_modal" data-meeting="{{$table->meeting_id}}">
-                <div>
-                    <h2>Seats: <span>2 Available<span></h2>
-                    <h2 class="mt-3">Seats: <span>2<span></h2>
-                </div>
-                <div class="mt-4">
-                    <h3 class="d-flex align-items-center"> 
-                        <span class="mr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" class="bi bi-person-check" viewBox="0 0 16 16">
-                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-                            </svg>
-                        </span> 
-                        Attendee User
-                    </h3>
-                </div>
-            </div>
-            <ul class="tob_Chair">
-                <li>
-                    <img src="{{asset("/assets/images/chair-svg.svg")}}" />
-                    <div class="chairBooking">
-                        <span>
-                            <img src="/assets/images/user_img.png" alt="">
-                        </span>
-                        <h4>User Name</h4>
-                    </div>
-                </li>
-            </ul>
-            <ul class="bottom_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-        </div>
-        <div class="table_Box">
-            <div class="TableBlock d-flex justify-content-between flex-column" data-toggle="modal" data-table="{{$table->id}}" data-target="#lounge_modal" data-meeting="{{$table->meeting_id}}">
-                <div>
-                    <h2>Seats: <span>2 Available<span></h2>
-                    <h2 class="mt-3">Seats: <span>2<span></h2>
-                </div>
-                <div class="mt-4">
-                    <h3 class="d-flex align-items-center"> 
-                        <span class="mr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" class="bi bi-person-check" viewBox="0 0 16 16">
-                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-                            </svg>
-                        </span> 
-                        Attendee User
-                    </h3>
-                </div>
-            </div>
-            <ul class="tob_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-            <ul class="right_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-            <ul class="bottom_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-            <ul class="left_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-        </div>
-        <div class="table_Box">
-            <div class="TableBlock d-flex justify-content-between flex-column" data-toggle="modal" data-table="{{$table->id}}" data-target="#lounge_modal" data-meeting="{{$table->meeting_id}}">
-                <div>
-                    <h2>Seats: <span>2 Available<span></h2>
-                    <h2 class="mt-3">Seats: <span>2<span></h2>
-                </div>
-                <div class="mt-4">
-                    <h3 class="d-flex align-items-center"> 
-                        <span class="mr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" class="bi bi-person-check" viewBox="0 0 16 16">
-                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-                            </svg>
-                        </span> 
-                        Attendee User
-                    </h3>
-                </div>
-            </div>
-            <ul class="tob_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-            <ul class="right_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-            <ul class="bottom_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-            <ul class="left_Chair">
-                <li><img src="{{asset("/assets/images/chair-svg.svg")}}" /></li>
-            </ul>
-        </div> --}}
+        
     </div>
     <div class="container-fluid">
-        <div class="table_Box w-100">
+        <div class="table_Box BigTableBox">
             <div class="TableBlock bigTable d-flex justify-content-between align-items-center">
                 <div>
                     <h2>Seats: <span>6 Available<span></span></span></h2>
@@ -505,16 +369,16 @@
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
-                <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
             </ul>
             <ul class="left_Chair bigLeftchair">
+                <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
             </ul>
             <ul class="right_Chair bigRightchair">
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
+                <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
             </ul>
             <ul class="bottom_Chair bigBottomchair">
-                <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
                 <li><img src="http://sourav-ball.localhost:8000/assets/images/chair-svg.svg"></li>
