0cc201dafcb000b78a04186ebd7b2406e0a05b1b
shubh palan <shubhpalan@gmail.com>
2022-02-22 18:54:01 +0530

    dropify

--- resources/views/includes/styles/fileUploader.blade.php
+++ resources/views/includes/styles/fileUploader.blade.php
@@ -1,5 +1,5 @@
-<link href="{{ asset("assets/libs/dropify/css/dropify.min.css") }}" rel="stylesheet" type="text/css" /> 
-{{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css" integrity="sha512-EZSUkJWTjzDlspOoPSpUFR0o0Xy7jdzW//6qhUkoZ9c4StFkVsp9fbbd0O06p9ELS3H486m4wmrCELjza4JEog==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+{{-- <link href="{{ asset("assets/libs/dropify/css/dropify.min.css") }}" rel="stylesheet" type="text/css" />  --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css" integrity="sha512-EZSUkJWTjzDlspOoPSpUFR0o0Xy7jdzW//6qhUkoZ9c4StFkVsp9fbbd0O06p9ELS3H486m4wmrCELjza4JEog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <style media="screen">
   .dropify-wrapper p{
     display: none;
