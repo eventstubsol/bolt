afbb68b68cad454cfd37bda9e51b4096c7f93de0
swarnadeep-pramanick <swarnadeeppramanick2@gmail.com>
2022-03-07 19:20:41 +0530

    activate account setting

--- app/Http/Controllers/AccountActiveController.php
+++ app/Http/Controllers/AccountActiveController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\Mail\RegistrationMail;
 use App\Event;
+use App\LoginLog;
+use App\Points;
 use App\Template;
 use Illuminate\Support\Facades\Mail as Mailing;
+use Carbon\Carbon;
 
 class AccountActiveController extends Controller
 {
@@ -30,9 +35,41 @@ class AccountActiveController extends Controller
         $template = Template::where('event_id',$event->id)->first();
         $template->message = str_replace('{user.name}',$user->name,$template->message);
         $template->message = str_replace('{user.email}',$user->email,$template->message);
-
+        $event= Event::where('slug',$subdomain)->first();
         Mailing::to($user->email)->send(new RegistrationMail($user,$template,$event));
-        flash("Registration Successful")->success();
-        return redirect()->route('thank.page',$subdomain);
+        if(Carbon::now()->setTimezone($event->timezone) < Carbon::parse($event->start_date)->setTimezone($event->timezone)){
+            flash("Registration Successful")->success();
+            return redirect()->route('thank.page',$subdomain);
+        }
+        else{
+            $user->online_status = 1;
+                $user->save();
+                DB::table("sessions")->where("user_id", $user->id)->whereNotIn("id", [session()->getId()])->delete();
+                Auth::login($user);
+                LoginLog::create(["ip" => $req->ip(), "user_id" => $user->id]);
+                // UserLocation::create(['user_id'=>$user->id,'event_id'=>$user->event_id,'type'=>"exterior"]);
+                $pointsDetails = [
+                    "points_to" => $user->id,
+                    "points_for" => "login",
+                    "details" => "",
+                    "points" => LOGIN_POINTS,
+                ];
+                if (!Points::where($pointsDetails)->count()) {
+                    Points::create($pointsDetails);
+                    $user->update([
+                        "points" => DB::raw('points+' . LOGIN_POINTS),
+                    ]);
+                }
+                //Users to whoom we have to notify about the recent login of current user
+                foreach ($user->tags as $tag) {
+                    foreach ($tag->looking_users as $looking_user) {
+                        $looking_user->sendNotification("suggested_user_login", "One of your suggested users just logged in. Visit attendees section to grow your network.", "info", $user->id);
+                    }
+                }
+                $user->touch();
+                // dd("test");
+
+                return redirect(route("eventee.event",['subdomain'=>$event->slug]));
+        }
     }
 }
--- resources/views/sessionrooms/edit.blade.php
+++ resources/views/sessionrooms/edit.blade.php
@@ -14,7 +14,7 @@
 
 
 @section("breadcrumbs")
-    <li class="breadcrumb-item"><a href="{{ route("sessionrooms.index") }}">Session Rooms</a></li>
+    <li class="breadcrumb-item"><a href="{{ route("eventee.sessionrooms.index",$id) }}">Session Rooms</a></li>
     <li class="breadcrumb-item active">Edit</li>
 @endsection
 
