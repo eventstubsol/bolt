fe0c7a943fe388d869dbba67d4bdb9df59a67d8c
shubh palan <shubhpalan@gmail.com>
2022-04-01 19:52:43 +0530

    cursor issue

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -49,11 +49,11 @@
     @include("includes.styles.sweetalert2")
     @include("includes.styles.fileUploader")
     <!-- Custom -->
-    <link href="{{ asset('/dflip/css/dflip.css') }}?cb=2187236762899" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/dflip/css/themify-icons.css') }}?cb=2187236762899" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}?cb=2187236762899" type="text/css">
-    <link rel="stylesheet" href="{{ asset('event-assets/css/app.css') }}?cb=2187236762899">
-    <link href="{{ asset('assets/css/custom.css') }}?v=2187236762899" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/dflip/css/dflip.css') }}?cb=2187236762828" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/dflip/css/themify-icons.css') }}?cb=2187236762828" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}?cb=2187236762828" type="text/css">
+    <link rel="stylesheet" href="{{ asset('event-assets/css/app.css') }}?cb=2187236762828">
+    <link href="{{ asset('assets/css/custom.css') }}?v=2187236762828" rel="stylesheet" type="text/css" />
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id={{ api('GA_TRACKING_ID',$event_id) }}"></script>
     @php
@@ -354,13 +354,13 @@
         window.config = config;
     </script>
     {{-- <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> --}}
-    <script src="{{ asset('assets/js/vendor.min.js') }}?cb=2187236762899"></script>
-    <script src="{{ asset('assets/js/app.min.js') }}?cb=2187236762899"></script>
-    <script src="{{ asset('event-assets/js/routie.min.js') }}?cb=2187236762899"></script>
-    <script src="{{ asset('event-assets/js/app.js') }}?cb=2187236762899"></script>
-    <script src="{{ asset('/js/profile/index.js') }}?cb=2187236762899"></script>
-    <script src="{{ asset('event-assets/YouTubePopUp/YouTubePopUp.jquery.js') }}?cb=2187236762899"></script>
-    <script src="{{ asset('event-assets/YouTubePopUp/PopupInit.js') }}?cb=2187236762899"></script>
+    <script src="{{ asset('assets/js/vendor.min.js') }}?cb=2187236762828"></script>
+    <script src="{{ asset('assets/js/app.min.js') }}?cb=2187236762828"></script>
+    <script src="{{ asset('event-assets/js/routie.min.js') }}?cb=2187236762828"></script>
+    <script src="{{ asset('event-assets/js/app.js') }}?cb=2187236762828"></script>
+    <script src="{{ asset('/js/profile/index.js') }}?cb=2187236762828"></script>
+    <script src="{{ asset('event-assets/YouTubePopUp/YouTubePopUp.jquery.js') }}?cb=2187236762828"></script>
+    <script src="{{ asset('event-assets/YouTubePopUp/PopupInit.js') }}?cb=2187236762828"></script>
     <script src="https://onesignal.github.io/emoji-picker/lib/js/config.js"></script>
     <script src="https://onesignal.github.io/emoji-picker/lib/js/util.js"></script>
     <script src="https://onesignal.github.io/emoji-picker/lib/js/jquery.emojiarea.js"></script>
