6b4d40bab717cde4b03e62d559c5b0d87abffe44
shubh palan <shubhpalan@gmail.com>
2022-04-04 21:36:15 +0530

    reg form fix

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="{{ asset("assets/css/log.css") }}?xyz=123">
     {{-- <link href="{{asset("assets/css/bootstrap-dark.min.css")}}" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" disabled="disabled"> --}}
     {{-- <link href="{{asset("assets/css/app-dark.min.css")}}" rel="stylesheet" type="text/css" id="app-dark-stylesheet" disabled="disabled"> --}}
-    <link href="{{ asset('assets/css/auth.css') }}?x=123" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/auth.css') }}?x=1234" rel="stylesheet" type="text/css" />
 
     <!-- icons -->
     <link href="{{asset("assets/css/icons.min.css")}}" rel="stylesheet" type="text/css">
