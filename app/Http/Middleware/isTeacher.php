8093064f33646ae3e02a0eb0dfd019d0cf02fd98
shubh palan <shubhpalan@gmail.com>
2022-05-02 13:28:57 +0530

    event bugfix

--- app/Http/Middleware/isTeacher.php
+++ app/Http/Middleware/isTeacher.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 use Illuminate\Support\Facades\Auth;
 
+use App\Event;
 
 use Closure;
 
@@ -18,9 +19,33 @@ class isTeacher
     public function handle($request, Closure $next)
     {
         $user = Auth::user();
+        if($request->route('id')){
+           $event_user = Event::find($request->route('id')) ? Event::find($request->route('id'))->user_id :'';
+           if($event_user===''){
+               return $next($request);
+           }
+           if($event_user===$user->id){
+               return $next($request);
+           }else{
+            abort(403);
+           }
+        }
+        if($request->route('event_id')){
+           $event_user = Event::find($request->route('event_id')) ? Event::find($request->route('event_id'))->user_id :'';
+           if($event_user===''){
+            return $next($request);
+        }
+           if($event_user===$user->id){
+               return $next($request);
+           }else{
+            abort(403);
+           }
+        }
+        // dd($request->route('id'));
         if(Auth::check() && ($user->type == 'eventee' || $user->type == 'admin')){
             return $next($request);
         }
+        
         return redirect('/');
         // abort(404);
     }
