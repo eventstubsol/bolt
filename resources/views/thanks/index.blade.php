7f4e3dee82111836475202d5eab928cad0807c26
EventStub <eventstubsol@gmail.com>
2022-07-27 12:07:03 +0530

    Time commented from Thank you Page

--- resources/views/thanks/index.blade.php
+++ resources/views/thanks/index.blade.php
@@ -30,7 +30,7 @@ Thank You
     <div class="d-flex align-items-center flex-column">
         <h5>Thank you registering for <b>{{ $event->name }}</b>. A registration confirmation has been sent to your regsitered email address. Please check your spam/junk email folder in case the email is not visible in your primary inbox. We look forward to seeing you at the event.</h5>
         @if(Carbon\Carbon::now() < Carbon\Carbon::parse($event->start_date))
-            <h3><b>Event Starts At : {{ Carbon\Carbon::parse($event->start_time)->format('H:i').  ' On '  .Carbon\Carbon::parse($event->start_date)->format('m-d-Y')}}</b></h3>
+            <!-- <h3><b>Event Starts At : {{ Carbon\Carbon::parse($event->start_time)->format('H:i').  ' On '  .Carbon\Carbon::parse($event->start_date)->format('m-d-Y')}}</b></h3> -->
         @endif
         <a href="{{ route('attendeeLogin',$subdomain) }}" style="background-color: {{ $event->primary_color }}; color:white;" class="btn btn-primary mt-2">Login Here</a>
     </div>
